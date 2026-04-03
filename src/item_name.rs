60a917ba84ed2d57ad23360ee767261c008340fe
S0ra <s0ra@duck.com>
2026-04-03 09:45:58 -0500

    Add the new F2 items, change some stuff, refactor some dumb code and realign everything WITH THE KING AND THE GOAT OF ALIGNMENT -> HELIX <-

--- src/item_name.rs
+++ src/item_name.rs
@@ -7,6 +7,7 @@ use crate::{
     items::{ITEMS, Item},
 };
 
+#[rustfmt::skip]
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum ItemName {
     // Weapon
@@ -20,8 +21,6 @@ pub enum ItemName {
     FireDagger,
     RefinedBlade,
     IceSpear,
-    // FrozenStar,
-    // Starfrost,
     Nightfall,
     LuminousObsidian,
     Wildfire,
@@ -30,10 +29,15 @@ pub enum ItemName {
     Lightspear,
     Sivoka,
     VoidDagger,
-    // Datafrost,
-    // Solaris,
     Kosa,
-    // GreatKosa,
+        // F2 //
+    RelicOfWinter,
+    GlacierGreatsword,
+    AbsoluteZeroSpear,
+    TitanBreakerSword,
+    FrostfangBlade,
+    CliffBreakerHammer,
+    IceBlade,
 
     // Armor
     StarterArmor,
@@ -45,6 +49,11 @@ pub enum ItemName {
     BloodCrystalArmor,
     VoidPlatemail,
     TheSingularity,
+        // F2 //
+    EternalFrostArmor,
+    GlacierArmor,
+    FrostshadeArmor,
+    IceplateArmor,
 
     // PassiveSkill
     RustyBlade,
@@ -54,7 +63,6 @@ pub enum ItemName {
     BladeArtist,
     VampiricWeapons,
     ArcaneVision,
-    // Pioneer,
 
     // ActiveSkill
     EmeraldSlash,
@@ -65,6 +73,9 @@ pub enum ItemName {
     MidnightEcho,
     BlazingEcho,
     VoidSlice,
+        // F2 //
+    FangSlash,
+    BlizzardEcho,
 
     // Material
     Amethyst,
@@ -88,6 +99,14 @@ pub enum ItemName {
     FallProtection,
     VoidCore,
     InvisibleCore,
+        // F2 //
+    AncientIceRelic,
+    GlacialFragment,
+    FrozenCore,
+    IceCrystal,
+    PackedSnow,
+    KnockbackCore,
+    FrostShard,
 
     // Aura
     DarkstarAura,
@@ -96,12 +115,9 @@ pub enum ItemName {
     ScarletPixelAura,
     SkyPixelAura,
     GrassPixelAura,
-    // ObsidianFlameAura,
-    // SunsetAura,
-    // FallingLeavesAura,
-    // SakuraFlameAura,
     SakuraAura,
-    // BetaAura,
+        // F2 //
+    BlizzardAura,
 
     // Shard
     CommonShard,
@@ -121,6 +137,7 @@ impl ItemName {
     #[must_use]
     pub fn display(self, locale: &str) -> Cow<'_, str> {
         match self {
+            // Weapons
             Self::BasicSword          => t!("item.basic_sword",          locale = locale),
             Self::VerdantDagger       => t!("item.verdant_dagger",       locale = locale),
             Self::RedstoneHammer      => t!("item.redstone_hammer",      locale = locale),
@@ -140,6 +157,16 @@ impl ItemName {
             Self::Sivoka              => t!("item.sivoka",               locale = locale),
             Self::VoidDagger          => t!("item.void_dagger",          locale = locale),
             Self::Kosa                => t!("item.kosa",                 locale = locale),
+                // F2 //
+            Self::RelicOfWinter       => t!("item.relic_of_winter",      locale = locale),
+            Self::GlacierGreatsword   => t!("item.glacier_greatsword",   locale = locale),
+            Self::AbsoluteZeroSpear   => t!("item.absolute_zero_spear",  locale = locale),
+            Self::TitanBreakerSword   => t!("item.titanbreaker_sword",   locale = locale),
+            Self::FrostfangBlade      => t!("item.frostfang_blade",      locale = locale),
+            Self::CliffBreakerHammer  => t!("item.cliff_breaker_hammer", locale = locale),
+            Self::IceBlade            => t!("item.ice_blade",            locale = locale),
+
+            // Armor
             Self::StarterArmor        => t!("item.starter_armor",        locale = locale),
             Self::ForestArmor         => t!("item.forest_armor",         locale = locale),
             Self::RedstoneArmor       => t!("item.redstone_armor",       locale = locale),
@@ -149,6 +176,13 @@ impl ItemName {
             Self::BloodCrystalArmor   => t!("item.blood_crystal_armor",  locale = locale),
             Self::VoidPlatemail       => t!("item.void_platemail",       locale = locale),
             Self::TheSingularity      => t!("item.the_singularity",      locale = locale),
+                // F2 //
+            Self::EternalFrostArmor   => t!("item.eternal_frost_armor",  locale = locale),
+            Self::GlacierArmor        => t!("item.glacier_armor",        locale = locale),
+            Self::FrostshadeArmor     => t!("item.frostshade_armor",     locale = locale),
+            Self::IceplateArmor       => t!("item.iceplate_armor",       locale = locale),
+
+            // Passive Skill
             Self::RustyBlade          => t!("item.rusty_blade",          locale = locale),
             Self::BattleFocus         => t!("item.battle_focus",         locale = locale),
             Self::SharpBlade          => t!("item.sharp_blade",          locale = locale),
@@ -156,6 +190,8 @@ impl ItemName {
             Self::BladeArtist         => t!("item.blade_artist",         locale = locale),
             Self::VampiricWeapons     => t!("item.vampiric_weapons",     locale = locale),
             Self::ArcaneVision        => t!("item.arcane_vision",        locale = locale),
+            
+            // Active Skill
             Self::EmeraldSlash        => t!("item.emerald_slash",        locale = locale),
             Self::WindyEdge           => t!("item.windy_edge",           locale = locale),
             Self::LifeSpark           => t!("item.life_spark",           locale = locale),
@@ -164,6 +200,11 @@ impl ItemName {
             Self::MidnightEcho        => t!("item.midnight_echo",        locale = locale),
             Self::BlazingEcho         => t!("item.blazing_echo",         locale = locale),
             Self::VoidSlice           => t!("item.void_slice",           locale = locale),
+                // F2 //
+            Self::FangSlash           => t!("item.fang_slash",           locale = locale),
+            Self::BlizzardEcho        => t!("item.blizzard_echo",        locale = locale),
+
+            // Material
             Self::Amethyst            => t!("item.amethyst",             locale = locale),
             Self::Obsidian            => t!("item.obsidian",             locale = locale),
             Self::Redstone            => t!("item.redstone",             locale = locale),
@@ -185,6 +226,16 @@ impl ItemName {
             Self::FallProtection      => t!("item.fall_protection",      locale = locale),
             Self::VoidCore            => t!("item.void_core",            locale = locale),
             Self::InvisibleCore       => t!("item.invisible_core",       locale = locale),
+                // F2 //
+            Self::AncientIceRelic     => t!("item.ancient_ice_relic",    locale = locale),
+            Self::GlacialFragment     => t!("item.glacial_fragment",     locale = locale),
+            Self::FrozenCore          => t!("item.frozen_core",          locale = locale),
+            Self::IceCrystal          => t!("item.ice_crystal",          locale = locale),
+            Self::PackedSnow          => t!("item.packed_snow",          locale = locale),
+            Self::KnockbackCore       => t!("item.knockback_core",       locale = locale),
+            Self::FrostShard          => t!("item.frost_shard",          locale = locale),
+
+            // Aura
             Self::DarkstarAura        => t!("item.darkstar_aura",        locale = locale),
             Self::CrystalBubbleAura   => t!("item.crystal_bubble_aura",  locale = locale),
             Self::LightningAura       => t!("item.lightning_aura",       locale = locale),
@@ -192,6 +243,10 @@ impl ItemName {
             Self::SkyPixelAura        => t!("item.sky_pixel_aura",       locale = locale),
             Self::GrassPixelAura      => t!("item.grass_pixel_aura",     locale = locale),
             Self::SakuraAura          => t!("item.sakura_aura",          locale = locale),
+                // F2 //
+            Self::BlizzardAura        => t!("item.blizzard_aura",        locale = locale),
+
+            // Shard
             Self::CommonShard         => t!("item.common_shard",         locale = locale),
             Self::UncommonShard       => t!("item.uncommon_shard",       locale = locale),
             Self::RareShard           => t!("item.rare_shard",           locale = locale),
@@ -200,86 +255,10 @@ impl ItemName {
         }
     }
 
+    #[inline]
     #[must_use]
-    pub fn to_str(self) -> &'static str {
-        match self {
-            Self::BasicSword          => "Basic Sword",
-            Self::VerdantDagger       => "Verdant Dagger",
-            Self::RedstoneHammer      => "Redstone Hammer",
-            Self::BalancedBlade       => "Balanced Blade",
-            Self::ObsidianSword       => "Obsidian Sword",
-            Self::BalancedSpear       => "Balanced Spear",
-            Self::VerdantGreatsword   => "Verdant Greatsword",
-            Self::FireDagger          => "Fire Dagger",
-            Self::RefinedBlade        => "Refined Blade",
-            Self::IceSpear            => "Ice Spear",
-            Self::Nightfall           => "Nightfall",
-            Self::LuminousObsidian    => "Luminous Obsidian",
-            Self::Wildfire            => "Wildfire",
-            Self::SpiritBlade         => "Spirit Blade",
-            Self::RubyBlade           => "Ruby Blade",
-            Self::Lightspear          => "Lightspear",
-            Self::Sivoka              => "Sivoka",
-            Self::VoidDagger          => "Void Dagger",
-            Self::Kosa                => "Kosa",
-            Self::StarterArmor        => "Starter Armor",
-            Self::ForestArmor         => "Forest Armor",
-            Self::RedstoneArmor       => "Redstone Armor",
-            Self::ObsidianArmor       => "Obsidian Armor",
-            Self::DraconicArmor       => "Draconic Armor",
-            Self::AmethystArmor       => "Amethyst Armor",
-            Self::BloodCrystalArmor   => "Blood Crystal Armor",
-            Self::VoidPlatemail       => "Void Platemail",
-            Self::TheSingularity      => "The Singularity",
-            Self::RustyBlade          => "Rusty Blade",
-            Self::BattleFocus         => "Battle Focus",
-            Self::SharpBlade          => "Sharp Blade",
-            Self::StoneHeart          => "Stone Heart",
-            Self::BladeArtist         => "Blade Artist",
-            Self::VampiricWeapons     => "Vampiric Weapons",
-            Self::ArcaneVision        => "Arcane Vision",
-            Self::EmeraldSlash        => "Emerald Slash",
-            Self::WindyEdge           => "Windy Edge",
-            Self::LifeSpark           => "Life Spark",
-            Self::CursedFlames        => "Cursed Flames",
-            Self::Genesis             => "Genesis",
-            Self::MidnightEcho        => "Midnight Echo",
-            Self::BlazingEcho         => "Blazing Echo",
-            Self::VoidSlice           => "Void Slice",
-            Self::Amethyst            => "Amethyst",
-            Self::Obsidian            => "Obsidian",
-            Self::Redstone            => "Redstone",
-            Self::Wood                => "Wood",
-            Self::Grass               => "Grass",
-            Self::CommonCore          => "Common Core",
-            Self::Ruby                => "Ruby",
-            Self::Firestone           => "Firestone",
-            Self::CursedWood          => "Cursed Wood",
-            Self::Leaf                => "Leaf",
-            Self::UncommonCore        => "Uncommon Core",
-            Self::VoidDust            => "Void Dust",
-            Self::SingularityFragment => "Singularity Fragment",
-            Self::BloodCrystal        => "Blood Crystal",
-            Self::EternalFirestone    => "Eternal Firestone",
-            Self::GlowingObsidian     => "Glowing Obsidian",
-            Self::PortalCore          => "Portal Core",
-            Self::RareCore            => "Rare Core",
-            Self::FallProtection      => "Fall Protection",
-            Self::VoidCore            => "Void Core",
-            Self::InvisibleCore       => "Invisible Core",
-            Self::DarkstarAura        => "Darkstar Aura",
-            Self::CrystalBubbleAura   => "Crystal Bubble Aura",
-            Self::LightningAura       => "Lightning Aura",
-            Self::ScarletPixelAura    => "Scarlet Pixel Aura",
-            Self::SkyPixelAura        => "Sky Pixel Aura",
-            Self::GrassPixelAura      => "Grass Pixel Aura",
-            Self::SakuraAura          => "Sakura Aura",
-            Self::CommonShard         => "Common Shard",
-            Self::UncommonShard       => "Uncommon Shard",
-            Self::RareShard           => "Rare Shard",
-            Self::EpicShard           => "Epic Shard",
-            Self::LegendaryShard      => "Legendary Shard",
-        }
+    pub fn to_str(self) -> Cow<'static, str> {
+        self.display("en-US")
     }
     
     pub fn from_str(s: &str) -> Res<Self> {
@@ -291,8 +270,8 @@ impl ItemName {
             .ok_or_else(|| format!("Unknown item: '{s}'").into())
     }
 
-    #[must_use]
     #[inline]
+    #[must_use]
     pub fn item(self) -> &'static Item {
         ITEMS.iter().find(|i| i.name == self).unwrap()
     }
